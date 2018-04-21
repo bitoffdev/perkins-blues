f459705ca6addf0c1810dcdd9dfbdc1e279ed557
Nitepone <admin@night.horse>
2018-04-21 18:38:18 -0400

    Structure for interfaces

---
+++ interfaces/controller.h
@@ -0,0 +1,12 @@
+/*
+ * controller.h
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+int add_animation(Animation animation);
+
+#endif /* !CONTROLLER_H */
