2d1eb69e179a2123322cd4eb858f706da04c3d37
Nitepone <admin@night.horse>
2018-05-03 20:17:53 +0000

    Trying to make splash animation work, getting close

--- src/splash_animation.py
+++ src/splash_animation.py
@@ -1,16 +1,8 @@
 import animation, colorsys
 
-def colorunpack(color):
-    color = int(color)
-    return ((color >> 16) / 255,
-            ((color >> 8) & 255) / 0xff,
-            (color & 0xff) / 0xff)
-def colorpack(color):
-    return sum(int(color[i] * 0xff) << (16 - 8*i) for i in range(3))
-
 class SplashAnimation(animation.Animation):
     """
-    Does a drip like animation from an epoch pocint to reach a new color
+    Does a drip like animation from an epoch point to reach a new color
     """
 
     def __init__(self, start_time, stop_time, start_pos, stop_pos, epoch_pos, start_color, stop_color):
@@ -28,9 +20,6 @@ class SplashAnimation(animation.Animation):
         self.set_start_pos(start_pos)
         self.set_stop_pos(stop_pos)
         
-        self.__start_hsv = colorsys.rgb_to_hsv(*colorunpack(start_color))
-        self.__stop_hsv = colorsys.rgb_to_hsv(*colorunpack(stop_color))
-
     def get_color(self, time, pos):
         """
         :param time: current time as seconds since the epoch
@@ -40,11 +29,11 @@ class SplashAnimation(animation.Animation):
         lerp = (time - self.get_start_time()) \
                 / (self.get_stop_time() - self.get_start_time())
         lerp = max(0, min(1, lerp))
-        math = ( abs(pos - epoch_pos)) ** (lerp * 3) 
-        if (math > .1):
-            return start_color;
+        weight = ( abs(pos - epoch_pos)) ** (lerp * 3) 
+        if (weight > .1):
+            return start_color
         else:
-            return stop_color;
+            return stop_color
 
 if __name__ == "__main__":
     import sys
